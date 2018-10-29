9be3a60ed0f85034c2e7107e955ad7792ed1cd03
FlorentViel <florviev@gmail.com>
2018-10-30 00:45:17 +0100

    update add_movie.php

--- add_movie.php
+++ add_movie.php
@@ -7,6 +7,7 @@ $currentPageTitle = "Ajoutez un film" ;
 require_once(__DIR__.'/partials/header.php');
 
 $title = $description = $video_link = $cover = null;
+$category = null;
 
 // le formulaire est soumis
 
@@ -22,27 +23,29 @@ if (!empty($_POST)) {
     }
     // Vérifier le description
     if (empty($description)) {
-        $errors['description'] = 'Le prix n\'est pas valide';
-     }
+        $errors['description'] = 'La description n\'est pas valide';
+    }
     // Vérifier la vidéo
     if ($video_link['error'] === 4) {
         $errors['video_link'] = 'L\'image n\'est pas valide';
     }
     // Vérifier la catégorie
-    if (empty($category) || !in_array($category, ['Action', 'Horreur', 'Aventure', 'Animation'])) {
-        $errors['category'] = 'La catégorie n\'est pas valide';
-    }
+    //if (empty($category) || !in_array($category, ['Action', 'Horreur', 'Aventure', 'Animation'])) {
+        //$errors['category_id'] = 'La catégorie n\'est pas valide';
+    //}
 
 
     // S'il n'y a pas d'erreurs dans le formulaire
     if (empty($errors)) {
         $query = $db->prepare('
-            INSERT INTO pizza (`title`, `description`, `video_link`, `category`) VALUES (:title, :description, :video_link, :category)
+            INSERT INTO movie (`title`, `description`, `video_link` `category_id` ) VALUES (:title, :description, :video_link, :category_id),
         ');
+
+        
         $query->bindValue(':title', $title, PDO::PARAM_STR);
         $query->bindValue(':description', $description, PDO::PARAM_STR);
         $query->bindValue(':video_link', $video_link, PDO::PARAM_STR);
-        $query->bindValue(':category', $category, PDO::PARAM_STR);
+        $query->bindValue(':name', $category, PDO::PARAM_STR);
         if ($query->execute()) { // On insère la pizza dans la BDD
             $success = true;
             // Envoyer un mail ?
@@ -68,19 +71,20 @@ if (!empty($_POST)) {
                         echo '</div>';
                     } ?>
                 </div>
-                <div class="form-group">
+             <div class="form-group">
                     <label for="description">Description :</label>
-                    <input type="text" name="description" id="description" class="form-control <?php echo isset($errors['description']) ? 'is-invalid' : null; ?>" value="<?php echo $description; ?>">
-                    <?php if (isset($errors['descritpion'])) {
+                    <textarea name="description" id="description" rows="5" class="form-control <?php echo isset($errors['description']) ? 'is-invalid' : null; ?>"><?php echo $description; ?></textarea>
+                    <?php if (isset($errors['description'])) {
                         echo '<div class="invalid-feedback">';
                             echo $errors['description'];
                         echo '</div>';
                     } ?>
                 </div>
+            </div>
                 <div class="form-group">
                     <label for="video_link">Lien du film :</label>
                     <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
-                    <input type="file" name="video_link" id="video_link" class="form-control <?php echo isset($errors['vdieo_link']) ? 'is-invalid' : null; ?>">
+                    <input type="text" name="video_link" id="video_link" class="form-control <?php echo isset($errors['vdieo_link']) ? 'is-invalid' : null; ?>">
                     <?php if (isset($errors['video_link'])) {
                         echo '<div class="invalid-feedback">';
                             echo $errors['video_link'];
@@ -90,26 +94,17 @@ if (!empty($_POST)) {
             </div>
             <div class="col-md-6">
                 <div class="form-group">
-                    <label for="category">Catégorie :</label>
-                    <select name="category" id="category" class="form-control <?php echo isset($errors['category']) ? 'is-invalid' : null; ?>">
+                    <label for="name">Catégorie :</label>
+                    <select name="name" id="name" class="form-control <?php echo isset($errors['name']) ? 'is-invalid' : null; ?>">
                         <option value="">Choisir la catégorie</option>
-                        <option <?php echo ($category === 'Classique') ? 'selected' : ''; ?> value="Classique">Classique</option>
-                        <option <?php echo ($category === 'Spicy') ? 'selected' : ''; ?> value="Spicy">Spicy</option>
-                        <option <?php echo ($category === 'Hot') ? 'selected' : ''; ?> value="Hot">Hot</option>
-                        <option <?php echo ($category === 'Végétarienne') ? 'selected' : ''; ?> value="Végétarienne">Végétarienne</option>
+                        <option <?php echo ($category === 'Action') ? 'selected' : ''; ?> value="Action">Action</option>
+                        <option <?php echo ($category === 'Horreur') ? 'selected' : ''; ?> value="Horreur">Horreur</option>
+                        <option <?php echo ($category === 'Aventure') ? 'selected' : ''; ?> value="Aventure">Aventure</option>
+                        <option <?php echo ($category === 'Animation') ? 'selected' : ''; ?> value="Animation">Animation</option>
                     </select>
-                    <?php if (isset($errors['category'])) {
+                    <?php if (isset($errors['name'])) {
                         echo '<div class="invalid-feedback">';
-                            echo $errors['category'];
-                        echo '</div>';
-                    } ?>
-                </div>
-                <div class="form-group">
-                    <label for="description">Description :</label>
-                    <textarea name="description" id="description" rows="5" class="form-control <?php echo isset($errors['description']) ? 'is-invalid' : null; ?>"><?php echo $description; ?></textarea>
-                    <?php if (isset($errors['description'])) {
-                        echo '<div class="invalid-feedback">';
-                            echo $errors['description'];
+                            echo $errors['name'];
                         echo '</div>';
                     } ?>
                 </div>
